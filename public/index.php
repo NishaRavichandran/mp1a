34279728d7d2a1ef552e0acc12ce9803e89276ed
nisha <nr437@njit.edu>
2019-02-20 15:53:00 -0500

    Using a factory(In Progress)

--- public/index.php
+++ public/index.php
@@ -5,4 +5,59 @@
  * Date: 2/19/19
  * Time: 2:32 PM
  */
-echo 'Hello World';
+main::start();
+class main {
+
+    Static public function start() {
+        $records=csv::getRecords();
+        $table=html::generateTable($records);
+        system::printPage($table);
+}
+}
+class csv{
+    Static public function getRecords() {
+        $make = 'Hyundai';
+        $model = 'I10';
+        $car = AutomobileFactory::create($make, $model);
+        print_r($car);
+
+        return $records;
+    }
+}
+
+class html{
+    Static public function generateTable($records) {
+        $table=$records;
+        return $table;
+
+
+    }}
+class system{
+    Static public function printPage($page) {
+        echo $page;
+    }
+}
+class Automobile
+{
+    private $vehicleMake;
+    private $vehicleModel;
+
+    public function __construct($make, $model)
+    {
+        $this->vehicleMake = $make;
+        $this->vehicleModel = $model;
+    }
+
+    public function getMakeAndModel()
+    {
+        return $this->vehicleMake . ' ' . $this->vehicleModel;
+    }
+}
+
+class AutomobileFactory
+{
+    public static function create($make, $model)
+    {
+        return new Automobile($make, $model);
+    }
+}
